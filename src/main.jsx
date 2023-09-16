7c3e614e1b24e8cbdb95f293d37d8c69ea682e49
chiefkarim <krimotop2016@gmail.com>
2023-09-16 11:19:25 +0100

    sda

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Index from 'pages/index.jsx'
+import Index from './pages/index.jsx'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import Item from 'pages/item.jsx'
-import CollectionItems from 'pages/collectionitems.jsx'
-import Collection from 'pages/collection.jsx'
+import CollectionItems from './pages/collectionitems.jsx'
+import Item from './pages/item.jsx'
+import Collection from './pages/collection.jsx'
 const router = createBrowserRouter([
   {
     path: '/',
