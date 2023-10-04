561c836d48fb160d4c6c0d6bc59d047667da8a9d
chiefkarim <krimotop2016@gmail.com>
2023-10-04 08:12:33 +0100

    Fix issue with log out

--- src/components/Log-Out.jsx
+++ src/components/Log-Out.jsx
@@ -1,11 +1,16 @@
 
+import { useDispatch } from "react-redux";
+import { UserLoggedOut } from "../redux/currentUserReducer";
+
 export default function LogOut(){
-function Logout(){
+    const dispatch = useDispatch()
+    function Logout(){
     const pastDate = new Date();
+    
     pastDate.setTime(pastDate.getTime() - 1);
 document.cookie='Authenticate=; expires='+pastDate.toUTCString()+'; path=/;'
-
+dispatch(UserLoggedOut())
 }
-    return( <a href='/' role="button" onClick={Logout} className=" lg:p-4 py-3   no-underline text-[#F5F5F5]  ">Log out</a>
+    return( <a href='/' onClick={Logout} className=" lg:p-4 py-3   no-underline text-[#F5F5F5]  ">Log out</a>
     )
 }
