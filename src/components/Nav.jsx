0e74b794a6b2acb27aae5fa98f857f09526f17ea
chiefkarim <krimotop2016@gmail.com>
2023-10-04 10:52:43 +0100

    fix nav bar UI

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -57,12 +57,12 @@ export default function Nav(){
  } )
 
     }
-    return(<nav className="lg:px-28 absolute lg:static lg:w-auto w-full  top-0  py-2 lg:flex lg:flex-row flex flex-col  justify-between  bg-[#3C3C34]">
-<Link role="button" to="/" className="text-5xl  lg:p-4  py-2 p-4 font-light no-underline lg:min-w-fit text-[#F5F5F5]">E-commerce</Link>        
+    return(<nav className="lg:px-28 absolute lg:static lg:w-auto w-full  top-0 py-3  lg:flex lg:flex-row flex flex-col  justify-between  bg-[#3C3C34]">
+<Link role="button" to="/" className="text-5xl  lg:p-4  py-2 p-4 font-thin no-underline lg:min-w-fit text-[#F5F5F5]">Home</Link>        
     <div className="mobileMenu hidden  lg:text-5xl lg:flex  w-full  text-center font-thin lg:flex-row  flex-col lg:justify-between ">
         
-<Link role="button" to="/collection/" className="py-2 lg:p-4  no-underline text-[#F5F5F5]">Collection</Link>        
-   {username === 'admin' ?   (<><Controllers/> <div className="py-3 lg:p-4">
+<Link role="button" to="/collection/" className="py-2 lg:py-4  no-underline text-[#F5F5F5]">Collection</Link>        
+   {username === 'admin' ?   (<><Controllers/> <div className="py-2 ">
    <button className="border-none bg-[#3C3C34]    w-full lg:w-auto"   onClick={toggleCart}> 
    
    <svg width="25px" height="25px"  className="Icon Icon--cart relative top-0.5 " role="presentation" viewBox="0 0 17 20" cursor="pointer"><path d="M0 20V4.995l1 .006v.015l4-.002V4c0-2.484 1.274-4 3.5-4C10.518 0 12 1.48 12 4v1.012l5-.003v.985H1V19h15V6.005h1V20H0zM11 4.49C11 2.267 10.507 1 8.5 1 6.5 1 6 2.27 6 4.49V5l5-.002V4.49z" fill="white"></path>
